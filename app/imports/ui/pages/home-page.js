eeb2213b6daf699076710a04efccd26036f7c893
Bryant Sanchez <bryantsa@hawaii.edu>
2017-10-26 23:24:26 -1000

    finished sorting

--- app/imports/ui/pages/home-page.js
+++ app/imports/ui/pages/home-page.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Contacts } from '../../api/contacts/contacts.js';
+import './tablesort.js';
 
 Template.Home_Page.helpers({
 
@@ -7,25 +8,31 @@ Template.Home_Page.helpers({
    * @returns {*} All of the Contacts documents.
    */
   contactsList() {
-    console.log(Contacts.find( { group: 'School' }).fetch());
+    //   console.log(Contacts.find({ group: 'School' }).fetch());
     return Contacts.find();
   },
   star(field, contact) {
     const contactData = Contacts.findOne(contact);
-    console.log(contactData);
+    //console.log(contactData);
     const icon = 'star icon';
     if (contactData[field] === true) {
       return icon;
     }
     return '';
   },
-  empty(field, contact){
-    
-    const group = Contacts.find( { group: 'School' }).fetch();
+  empty(field, contact) {
+
+    const group = Contacts.find({ group: 'School' }).fetch();
     if (group) {
 
     }
   }
+});
+
+Template.Home_Page.events({
+  'click.sortable-table'() {
+    $('table').tablesort();
+  },
 });
 
 Template.Home_Page.onCreated(function onCreated() {
